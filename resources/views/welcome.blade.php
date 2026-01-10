697091580ee307f56a213ca72004b5e2f31379cf
Felixgunawan1234 <212820344+Felixganteng1234@users.noreply.github.com>
2026-01-10 16:09:43 +0700

    Fix alwi college

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -192,7 +192,7 @@
               </div>
 
       <!-- Features Section -->
-      <div id="features" class="relative -mt-28 sm:-mt-16 md:-mt-20 lg:-mt-20 scroll-mt-24 sm:scroll-mt-28 bg-white">
+      <div id="features" class="relative mt-6 sm:mt-8 md:mt-10 lg:mt-12 scroll-mt-24 sm:scroll-mt-28 bg-white">
         <section class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-0 sm:pt-4 pb-10">
           <h2 class="text-2xl sm:text-3xl font-semibold sm:font-bold text-center text-[#3B63B5] heading-accent"><span class="heading-inline">Mengapa Alwi College?</span></h2>
           <p class="text-center text-gray-600 text-base sm:text-lg mt-2 sm:mt-3 max-w-2xl mx-auto">Belajar jadi lebih fokus, menyenangkan, dan terarah dengan pendampingan intensif serta materi yang dirancang untuk hasil terbaik.</p>
